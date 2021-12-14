e2ab31e6cb4b7da08f67f2f0c3569db06b4f526c
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2021-12-14 10:11:27 +0530

    Tags functionality added

--- src/components/QuestionAnswersCSVUpload.js
+++ src/components/QuestionAnswersCSVUpload.js
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useRef, useState, useCallback } from "react";
 import readXlsxFile from "read-excel-file";
-import { Table } from "reactstrap";
+import ReactTags from "react-tag-autocomplete";
+import { Table, Input, Container, Row, Col } from "reactstrap";
 export default function CSVReader(props) {
   // const { isheader, isSrNo, answerColumnIndex, headerProps } = props;
   const [csvFile, setCsvFile] = useState();
   const [headers, setHeaders] = useState([]);
   const [csvArray, setCsvArray] = useState([]);
   const [options, setOptions] = useState([]);
+  const [tagsList, setTagsList] = useState([]);
+  const [tags, setTags] = useState();
+  const [mainTags, setMainTags] = useState();
+  const reactTags = useRef();
   // [{name: "", age: 0, rank: ""},{name: "", age: 0, rank: ""}]
   const main = async () => {
     const rows = await readXlsxFile(csvFile);
@@ -14,7 +19,7 @@ export default function CSVReader(props) {
     setHeaders(rows[0]);
     const opt = [];
     rows[0].forEach((el) => {
-      debugger;
+      el = el || "";
       if (el.toLowerCase().includes("option ")) {
         opt.push(el.toLowerCase().replace("option ", "").toLowerCase());
       }
@@ -22,7 +27,68 @@ export default function CSVReader(props) {
     setOptions(opt);
     processCSV(rows[0], rows.slice(1));
   };
+  const onDelete = useCallback(
+    (i, tagIndex) => {
+      const prevTags = tags ? { ...tags } : {};
+      prevTags[i] = prevTags[i].filter((_, i) => i !== tagIndex);
+      setTags(prevTags);
+    },
+    [tags]
+  );
 
+  const onAddition = useCallback(
+    (i, newTag) => {
+      const prevTags = tags ? { ...tags } : {};
+      const prevTagsList = [...tagsList];
+      if (
+        !prevTagsList.some(
+          (s) => s.name.toLowerCase() === newTag.name.toLowerCase()
+        )
+      ) {
+        prevTagsList[prevTagsList.length] = newTag;
+        setTagsList(prevTagsList);
+      }
+      if (
+        prevTags[i].some(
+          (s) => s.name.toLowerCase() === newTag.name.toLowerCase()
+        )
+      )
+        return;
+      prevTags[i] = [...prevTags[i], newTag];
+      setTags(prevTags);
+    },
+    [tags]
+  );
+  const onDeleteMain = useCallback(
+    (tagIndex) => {
+      const prevTags = mainTags ? [...mainTags] : [];
+      prevTags = prevTags.filter((_, i) => i !== tagIndex);
+      setMainTags(prevTags);
+    },
+    [tags]
+  );
+
+  const onAdditionMain = useCallback(
+    (newTag) => {
+      let prevTags = mainTags ? [...mainTags] : [];
+      const prevTagsList = [...tagsList];
+      if (
+        !prevTagsList.some(
+          (s) => s.name.toLowerCase() === newTag.name.toLowerCase()
+        )
+      ) {
+        prevTagsList[prevTagsList.length] = newTag;
+        setTagsList(prevTagsList);
+      }
+      if (
+        prevTags.some((s) => s.name.toLowerCase() === newTag.name.toLowerCase())
+      )
+        return;
+      prevTags = [...prevTags, newTag];
+      setMainTags(prevTags);
+    },
+    [mainTags]
+  );
   const processCSV = (hdrs, rows) => {
     const newArray = rows.map((row) => {
       const eachObject = hdrs.reduce((obj = {}, header, i) => {
@@ -32,7 +98,10 @@ export default function CSVReader(props) {
           value = value.map((v) => v.trim());
           obj.single = value.length === 1;
         }
-
+        if (header === "Tags" && value) {
+          value = value.split(",");
+          value = value.map((v) => v.trim());
+        }
         obj[header] = value;
 
         // obj[header] = values[i].slice(0, values[i].indexOf("\r"));
@@ -41,7 +110,33 @@ export default function CSVReader(props) {
       return eachObject;
     });
     setCsvArray(newArray);
+    const tagsArr = {};
+    newArray.map((item, index) => {
+      if (item && item.Tags) {
+        const tagsArray = item.Tags.map((it, i) => {
+          return { id: i, name: it };
+        });
+        tagsArr[index] = tagsArray;
+      } else {
+        tagsArr[index] = [];
+      }
+    });
+    const tagsSuggestionsArr = [];
+    newArray.forEach((item, index) => {
+      if (item.Tags && item.Tags.length > 0) {
+        item.Tags.forEach((t) => {
+          if (!tagsSuggestionsArr.some((it) => it.name === t)) {
+            tagsSuggestionsArr.push({ id: tagsSuggestionsArr.length, name: t });
+          }
+        });
+      }
+    });
+    setTagsList(tagsSuggestionsArr);
+    setMainTags(tagsSuggestionsArr);
+    setTags(tagsArr);
   };
+  console.log("tagsList", tagsList);
+  console.log("tags", tags);
   const submit = () => {
     main();
     // const file = csvFile;
@@ -60,31 +155,60 @@ export default function CSVReader(props) {
   console.log(csvArray.length > 0);
   console.log("options", options);
   return (
-    <form id="csv-form">
-      <input
+    <>
+      <form id="csv-form" className="col-xs-9">
+        <Container className="mt-5">
+          <Row>
+            <Col sm={11}>
+              <Input
+                name="file"
+                type="file"
+                accept=".xlsx"
+                id="csvFile"
+                onClick={(e) => setCsvArray([])}
+                onChange={(e) => {
+                  setCsvFile(e.target.files[0]);
+                }}
+              />
+              {/* <input
         type="file"
-        accept=".xlsx"
-        id="csvFile"
-        onChange={(e) => {
-          setCsvFile(e.target.files[0]);
-        }}
-      ></input>
-      <br />
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          if (csvFile) submit();
-        }}
-      >
-        Submit
-      </button>
+      ></input> */}
+              {/* <br /> */}
+            </Col>
+            <Col sm={1}>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  if (csvFile) submit();
+                }}
+                className="col-xs-2"
+              >
+                Submit
+              </button>
+            </Col>
+          </Row>
+          {tagsList && tagsList.length > 0 && (
+            <Row>
+              <ReactTags
+                ref={reactTags}
+                minQueryLength={1}
+                tags={mainTags ? mainTags : []}
+                allowNew
+                suggestions={tagsList || []}
+                onDelete={onDeleteMain}
+                onAddition={onAdditionMain}
+              />
+            </Row>
+          )}
+        </Container>
+      </form>
       <br />
       <br />
       {csvArray.length > 0 ? (
         <>
           <Table bordered striped hover>
             <thead>
-              <tr>
+              <tr className="question-header">
                 {headers.map((h, index) => {
                   return <th>{h}</th>;
                 })}
@@ -95,7 +219,27 @@ export default function CSVReader(props) {
                 return (
                   <tr key={i}>
                     {headers.map((h, index) => {
-                      return <td>{item[headers[index]]}</td>;
+                      if (index === 0)
+                        return (
+                          <td className="question-first-col">
+                            {item[headers[index]]}
+                          </td>
+                        );
+                      if (h === "Tags") {
+                        return (
+                          <td>
+                            <ReactTags
+                              ref={reactTags}
+                              minQueryLength={1}
+                              tags={tags ? tags[i] : []}
+                              allowNew
+                              suggestions={tagsList || []}
+                              onDelete={onDelete.bind(null, i)}
+                              onAddition={onAddition.bind(null, i)}
+                            />
+                          </td>
+                        );
+                      } else return <td>{item[headers[index]]}</td>;
                     })}
                   </tr>
                 );
@@ -104,6 +248,6 @@ export default function CSVReader(props) {
           </Table>
         </>
       ) : null}
-    </form>
+    </>
   );
 }
